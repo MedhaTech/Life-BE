eb4690dc3f801235517a544771a34cd664824c16
Ramant <ramant@inqui-lab.org>
2024-03-22 12:49:20 +0530

    group by fix r1

--- src/controllers/report.controller.ts
+++ src/controllers/report.controller.ts
@@ -1511,6 +1511,9 @@ GROUP BY idea_id;`, { type: QueryTypes.SELECT });
             if (district) {
                 wherefilter = `WHERE org.state= '${district}'`;
             }
+            await db.query(`SET sql_mode=(SELECT REPLACE(@@sql_mode,'ONLY_FULL_GROUP_BY',''));`, {
+                type: QueryTypes.RAW,
+            });
             const summary = await db.query(`
             SELECT 
     institution_code,
